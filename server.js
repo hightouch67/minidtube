cd84b3cf9fa06ceac38e3816cf7a4d6caaba151e
hightouch <hightouch@hotmail.fr>
2018-05-28 18:30:51 +0200

    update

--- server.js
+++ server.js
@@ -13,15 +13,15 @@ const crawlers = jsonfile.readFileSync(file)
 // currently whitelisting a few robots
 // const allowedRobots = ['facebookexternalhit', 'Discordbot', 'Slackbot'
 //     , 'bingbot', 'Twitterbot']
-const rootDomain = 'https://d.tube'
+const rootDomain = 'https://fundition.io'
 
 const lightrpc = createClient('https://api.steemit.com');
 
 let layouts = {}
 
-app.use('/DTube_files', express.static(path.join(__dirname, 'static/production/DTube_files')))
-app.use('/favicon.ico', express.static(path.join(__dirname, 'static/production/DTube_files/images/dtubefavicon.png')))
-app.get('*', function(req, res, next) {
+app.use('/images', express.static(path.join(__dirname, 'http://www.fundition.io/images/')))
+app.use('/favicon.ico', express.static(path.join(__dirname, 'http://www.fundition.io/images/fundition.png')))
+app.get('*', function (req, res, next) {
     var isRobot = getRobotName(req.headers['user-agent'])
 
     // parsing the query
@@ -38,52 +38,39 @@ app.get('*', function(req, res, next) {
 
     if (isRobot)
         console.log(isRobot, 'GET', req.path, req.query)
-    
-    if (isRobot && reqPath.startsWith('/v/')) {
-        // DIRTY ROBOTS
-        getVideoHTML(
+    // DIRTY ROBOTS
+    console.log(reqPath)
+    getprojectHTML(
         reqPath.split('/')[2],
         reqPath.split('/')[3],
-        function(err, contentHTML, pageTitle, description, url, snap, urlvideo, duration, embedUrl) {
+        function (err, contentHTML, pageTitle, description, url, snap, urlvideo, duration, embedUrl) {
             if (error(err, next)) return
-            getRobotHTML(function(err, baseHTML) {
+            getRobotHTML(function (err, baseHTML) {
                 if (error(err, next)) return
                 baseHTML = baseHTML.replace(/@@CONTENT@@/g, contentHTML)
                 baseHTML = baseHTML.replace(/@@TITLE@@/g, htmlEncode(pageTitle))
                 baseHTML = baseHTML.replace(/@@DESCRIPTION@@/g, htmlEncode(description))
                 baseHTML = baseHTML.replace(/@@URL@@/g, htmlEncode(url))
-                baseHTML = baseHTML.replace(/@@URLNOHASH@@/g, htmlEncode(url).replace('/#!',''))
+                baseHTML = baseHTML.replace(/@@URLNOHASH@@/g, htmlEncode(url).replace('/#!', ''))
                 // facebook minimum snap is 200x200 otherwise useless
                 baseHTML = baseHTML.replace(/@@SNAP@@/g, htmlEncode(snap))
                 baseHTML = baseHTML.replace(/@@VIDEO@@/g, htmlEncode(urlvideo))
                 baseHTML = baseHTML.replace(/@@EMBEDURL@@/g, htmlEncode(embedUrl))
                 if (duration) {
                     var durationHTML = '<meta property="og:video:duration" content="@@VIDEODURATION@@" />'
-                    durationHTML = durationHTML.replace(/@@VIDEODURATION@@/g, htmlEncode(""+Math.round(duration)))
+                    durationHTML = durationHTML.replace(/@@VIDEODURATION@@/g, htmlEncode("" + Math.round(duration)))
                     baseHTML = baseHTML.replace(/@@METAVIDEODURATION@@/g, durationHTML)
                 } else {
                     baseHTML = baseHTML.replace(/@@METAVIDEODURATION@@/g, '')
                 }
-                
+
                 res.send(baseHTML)
             })
         })
-    } else {
-        // HUMAN BROWSER
-        // AND DISALLOWED ROBOTS
-        if (reqPath == '/') {
-            getHumanHTML(function(err, humanHTML) {
-                if (error(err, next)) return
-                res.send(humanHTML)
-            })
-        } else {
-            res.redirect('/#!'+reqPath);
-        }
-    }
-    
+
 })
 
-app.listen(port, () => console.log('minidtube listening on port '+port))
+app.listen(port, () => console.log('minidtube listening on port ' + port))
 
 function error(err, next) {
     if (err) {
@@ -100,7 +87,7 @@ function getRobotHTML(cb) {
         return
     }
     else {
-        fs.readFile(path.join(__dirname,"static","robots.html"), 'utf8', function (err,data) {
+        fs.readFile(path.join(__dirname, "static", "robots.html"), 'utf8', function (err, data) {
             if (err) {
                 cb(err)
                 return
@@ -113,32 +100,14 @@ function getRobotHTML(cb) {
     }
 }
 
-function getHumanHTML(cb) {
-    if (layouts.human) {
-        cb(null, layouts.human)
-        return
-    } else {
-        fs.readFile(path.join(__dirname,"static","production","index.html"), 'utf8', function (err,data) {
-            if (err) {
-                cb(err)
-                return
-            } else {
-                layouts.human = data
-                cb(null, data)
-                return
-            }
-        });
-    }
-}
-
-function getVideoHTML(author, permlink, cb) {
-    lightrpc.send('get_state', [`/dtube/@${author}/${permlink}`], function(err, result) {
+function getprojectHTML(author, permlink, cb) {
+    lightrpc.send('get_state', [`/@${author}/${permlink}`], function (err, result) {
         if (err) {
             cb(err)
             return
         }
-        //console.log(result.content[author+'/'+permlink])
-        var video = parseVideo(result.content[author+'/'+permlink])
+        console.log(result)
+        var video = parseVideo(result.content[author + '/' + permlink])
         if (!video.content || !video.info) {
             cb('Weird error')
             return;
@@ -148,17 +117,17 @@ function getVideoHTML(author, permlink, cb) {
         var downvotedBy = []
         for (let i = 0; i < video.active_votes.length; i++) {
             if (parseInt(video.active_votes[i].rshares) > 0)
-                upvotedBy.push(video.active_votes[i].voter);    
+                upvotedBy.push(video.active_votes[i].voter);
             if (parseInt(video.active_votes[i].rshares) < 0)
-                downvotedBy.push(video.active_votes[i].voter);         
+                downvotedBy.push(video.active_votes[i].voter);
         }
 
         var html = ''
-        html += '<video src="https://ipfs.io/ipfs/'+hashVideo+'" poster="https://ipfs.io/ipfs/'+video.info.snaphash+'" controls></video><br />'
-        html += '<h1>'+video.info.title+'</h1>'
-        html += '<h2>Author: '+video.info.author+'</h2>'
-        html += '<h2>Date: '+video.created.split('T')[0]+'</h2>'
-        html += '<p><strong>Description: </strong>'+video.content.description.replace(/(?:\r\n|\r|\n)/g, '<br />')+'</p>'
+        html += '<video src="https://ipfs.io/ipfs/' + hashVideo + '" poster="https://ipfs.io/ipfs/' + video.info.snaphash + '" controls></video><br />'
+        html += '<h1>' + video.info.title + '</h1>'
+        html += '<h2>Author: ' + video.info.author + '</h2>'
+        html += '<h2>Date: ' + video.created.split('T')[0] + '</h2>'
+        html += '<p><strong>Description: </strong>' + video.content.description.replace(/(?:\r\n|\r|\n)/g, '<br />') + '</p>'
         if (upvotedBy.length > 0) {
             html += '<p><strong>Upvoted by: </strong>'
             html += upvotedBy.join(', ')
@@ -169,11 +138,11 @@ function getVideoHTML(author, permlink, cb) {
             html += downvotedBy.join(', ')
             html += '</p>'
         }
-        
-        var url = rootDomain+'/#!/v/'+video.info.author+'/'+video.info.permlink
-        var snap = 'https://ipfs.io/ipfs/'+video.info.snaphash
-        var urlVideo = 'https://ipfs.io/ipfs/'+hashVideo
-        var embedUrl = 'https://emb.d.tube/#!/'+video.info.author+'/'+video.info.permlink+'/true'
+
+        var url = rootDomain + '/#!/v/' + video.info.author + '/' + video.info.permlink
+        var snap = 'https://ipfs.io/ipfs/' + video.info.snaphash
+        var urlVideo = 'https://ipfs.io/ipfs/' + hashVideo
+        var embedUrl = 'https://emb.d.tube/#!/' + video.info.author + '/' + video.info.permlink + '/true'
         var duration = video.info.duration || null
         var description = video.content.description.replace(/(?:\r\n|\r|\n)/g, ' ').substr(0, 300)
         cb(null, html, video.info.title, description, url, snap, urlVideo, duration, embedUrl)
@@ -182,8 +151,8 @@ function getVideoHTML(author, permlink, cb) {
 
 function parseVideo(video, isComment) {
     try {
-      var newVideo = JSON.parse(video.json_metadata).video
-    } catch(e) {
+        var newVideo = JSON.parse(video.json_metadata).video
+    } catch (e) {
         console.log(e)
     }
     if (!newVideo) newVideo = {}
