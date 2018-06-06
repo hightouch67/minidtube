c1ad5f08ea2f8bfaaf0613ee009ede8f0b921fa4
Hightouch <cetinkaya.ozden@gmail.com>
2018-06-06 23:50:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -88,7 +88,8 @@ function getRobotHTML(cb) {
 }
 
 function getVideoHTML(author, permlink, cb) {
-    console.log("hhe " + author+permlink])
+    console.log("hhe " + author + permlink)
+     console.log("hhe a" + [`/${author}/${permlink}`])
     lightrpc.send('get_state', [`/${author}/${permlink}`], function(err, result) {
         if (err) {
             cb(err)
