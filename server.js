0f05e28f24b538341f4fd47b73df0b1fdfb39f6b
Hightouch <cetinkaya.ozden@gmail.com>
2018-06-06 22:03:35 +0200

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const lightrpc = createClient('https://api.steemit.com');
 
 let layouts = {}
 console.log(app)
-/app.use('/images', express.static(path.join(__dirname, 'http://www.fundition.io/images/')))
+//app.use('/images', express.static(path.join(__dirname, 'http://www.fundition.io/images/')))
  app.use('/favicon.ico', ('http://www.fundition.io/images/fundition.png'))
 app.get('*', function (req, res, next) {
     var isRobot = getRobotName(req.headers['user-agent'])
