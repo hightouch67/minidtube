8b96af620326210d21f0cc1b60a2fcaf4ceebf7e
Hightouch <cetinkaya.ozden@gmail.com>
2018-06-06 22:22:09 +0200

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const lightrpc = createClient('https://api.steemit.com');
 let layouts = {}
 console.log(app)
 app.use('/files', express.static(path.join(__dirname, 'public/files')))
-app.use(favicon(path.join(__dirname,'public','files','fnd.png')));
+app.use('/favicon.ico', express.static(path.join(__dirname, 'public/files/fnd.png')))
 app.get('*', function (req, res, next) {
     var isRobot = getRobotName(req.headers['user-agent'])
     console.log(req,res,next)
