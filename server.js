8dd5ed35fb6cf0fd8705254eb7ad4866a438a260
hightouch <hightouch@hotmail.fr>
2018-10-06 11:35:18 +0200

    textcleaner

--- server.js
+++ server.js
@@ -125,7 +125,7 @@ function parseProject(project, isComment) {
     }
     if (!newProject) newProject = {}
     newProject.author = project.author
-    newProject.body = project.body
+    newProject.body = cleanText(project.body)
     newProject.total_payout_value = project.total_payout_value
     newProject.curator_payout_value = project.curator_payout_value
     newProject.pending_payout_value = project.pending_payout_value
@@ -160,3 +160,41 @@ function getThumbnail(string){
             }
         }
     }}
+
+
+    function cleanText(text){
+            if (!text) return text;
+            var urlPattern = /\b(?:https?|ftp):\/\/[a-z0-9-+&@#\/%?=~_|!:,.;]*[a-z0-9-+&@#\/%=~_|]/gim;
+            text = text.replace(urlPattern, "")
+            text = text.replace(/<img[^>"']*((("[^"]*")|('[^']*'))[^"'>]*)*>/g, "");
+            text = text.replace(/<(?:.|\n)*?>/gm, '');
+            //-- remove BR tags and replace them with line break
+            text = text.replace(/<br>/gi, "");
+            text = text.replace(/<br\s\/>/gi, "");
+            text = text.replace(/<br\/>/gi, "");
+            text = text.replace(/<iframe(.+)<\/iframe>/g, "");
+            //-- remove P and A tags but preserve what's inside of them
+            text = text.replace(/<p.*>/gi, "");
+            text = text.replace(/<a.*href="(.*?)".*>(.*?)<\/a>/gi, " $2 ($1)");
+        
+            //-- remove all inside SCRIPT and STYLE tags
+            text = text.replace(/<script.*>[\w\W]{1,}(.*?)[\w\W]{1,}<\/script>/gi, "");
+            text = text.replace(/<style.*>[\w\W]{1,}(.*?)[\w\W]{1,}<\/style>/gi, "");
+            //-- remove all else
+            text = text.replace(/<(?:.|\s)*?>/g, "");
+        
+            //-- get rid of more than 2 multiple line breaks:
+            text = text.replace(/(?:(?:\r\n|\r|\n)\s*){2,}/gim, "");
+        
+            //-- get rid of more than 2 spaces:
+            text = text.replace(/ +(?= )/g, '');
+        
+            //-- get rid of html-encoded characters:
+            text = text.replace(/&nbsp;/gi, " ");
+            text = text.replace(/&amp;/gi, "&");
+            text = text.replace(/&quot;/gi, '"');
+            text = text.replace(/&lt;/gi, '<');
+            text = text.replace(/&gt;/gi, '>');
+            text = text.replace(/\.[^/.]+$/, "")
+            return text;
+    }
